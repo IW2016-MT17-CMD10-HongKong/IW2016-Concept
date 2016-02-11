034f686868573a03314a9899fe6e6e027f3f2d11
Ian Wensink <ian@burst-digital.com>
2016-02-11 11:09:02 +0100

    JavaScript team section

--- js/script.js
+++ js/script.js
@@ -1,5 +1,12 @@
 function initApp() {
     $(".team-member").on("click", toggleActiveTeamMember);
+    var firstMember = $(".team-pictures-row:first-child .team-member:first-child");
+    firstMember.addClass("show active");
+    var paragraph = $("p[data-person=" + firstMember.data("person") + "]");
+    paragraph.addClass("show active");
+    setTimeout(function () {
+        paragraph.parent().height(paragraph.height());
+    }, 10);
 }
 
 function toggleActiveTeamMember() {
@@ -8,15 +15,23 @@ function toggleActiveTeamMember() {
     if (member[0] == currentMember[0]) return;
     var row = member.parent(".team-pictures-row");
     var currentRow = $(".team-pictures-row.active");
+    var paragraph = $("p[data-person=" + member.data("person") + "]");
+    var currentParagraph = $("p[data-person=" + currentMember.data("person") + "]");
     currentMember.removeClass("active");
+    currentParagraph.removeClass("active");
     if (row.index() != currentRow.index()) currentRow.removeClass("active");
+    currentParagraph.parent().height(0);
     setTimeout(function () {
         currentMember.removeClass("show");
-        $("");
+        currentParagraph.removeClass("show");
         member.addClass("show");
+        paragraph.addClass("show");
         setTimeout(function () {
             row.addClass("active");
             member.addClass("active");
+            paragraph.addClass("active");
+            paragraph.parent().height(paragraph.height());
+            $(".mdl-layout__content").animate({scrollTop: row.position().top}, 500);
         }, 10);
     }, 300);
 }
