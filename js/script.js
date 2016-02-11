b505b833756074b450570bba90100c6e85f9d866
Ian Wensink <ian@burst-digital.com>
2016-02-11 10:30:26 +0100

    Pauperniels

--- js/script.js
+++ js/script.js
@@ -1,3 +1,22 @@
 function initApp() {
+    $(".team-member").on("click", toggleActiveTeamMember);
+}
 
+function toggleActiveTeamMember() {
+    var member = $(this);
+    var currentMember =  $(".team-member.active");
+    if (member[0] == currentMember[0]) return;
+    var row = member.parent(".team-pictures-row");
+    var currentRow = $(".team-pictures-row.active");
+    currentMember.removeClass("active");
+    if (row.index() != currentRow.index()) currentRow.removeClass("active");
+    setTimeout(function () {
+        currentMember.removeClass("show");
+        $("");
+        member.addClass("show");
+        setTimeout(function () {
+            row.addClass("active");
+            member.addClass("active");
+        }, 10);
+    }, 300);
 }
